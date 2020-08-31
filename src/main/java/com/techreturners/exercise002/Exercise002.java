fae975018e44cb54de903e2401a71c7284b4e3bc
Jamie Johnstone <j@miej.co>
2020-08-31 18:23:08 +0100

    Uncomment parameters in methods for exercise002

--- src/main/java/com/techreturners/exercise002/Exercise002.java
+++ src/main/java/com/techreturners/exercise002/Exercise002.java
@@ -2,11 +2,11 @@ package com.techreturners.exercise002;
 
 public class Exercise002 {
 
-    public boolean isFromManchester(/* Person person */) {
+    public boolean isFromManchester(Person person) {
         return false;
     }
 
-    public boolean canWatchFilm(/* Person person, int ageLimit*/) {
+    public boolean canWatchFilm(Person person, int ageLimit) {
         return false;
     }
     
