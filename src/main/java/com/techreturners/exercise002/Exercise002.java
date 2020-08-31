dc28ee81959222b1e8ad9bf5058a81a61f7a60e2
Jamie Johnstone <j@miej.co>
2020-08-31 18:24:52 +0100

    Add isFromManchester() and canWatchFilm() methods

--- src/main/java/com/techreturners/exercise002/Exercise002.java
+++ src/main/java/com/techreturners/exercise002/Exercise002.java
@@ -3,11 +3,11 @@ package com.techreturners.exercise002;
 public class Exercise002 {
 
     public boolean isFromManchester(Person person) {
-        return false;
+        return person.getCity().equals("Manchester");
     }
 
     public boolean canWatchFilm(Person person, int ageLimit) {
-        return false;
+        return person.getAge() >= ageLimit;
     }
     
 }
